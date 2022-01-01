ec8da706b873ba1d00914dfa1c975c43aba5f0f3
barua-raj <barua.jovraj@gmail.com>
2022-01-02 02:06:07 +0600

    area-of-a-triangle program added

---
+++ fundamental_computer_programs/area-of-a-triangle.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Program Name: Area of a triangle</title>
+	</head>
+	<body>
+	
+		<?php
+			
+			$base = 4;
+			$height = 10;
+			$area_triangle = ($base*$height)/2;
+			
+			echo '<h1>Area of a triangle of base '.$base.' and height '.$height.' is '.$area_triangle.'.</h1>';
+			
+		?>
+	
+	</body>
+	
+</html>
