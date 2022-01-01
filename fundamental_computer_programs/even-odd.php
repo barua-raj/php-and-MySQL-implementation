2ab9066629129e9c4ebc8796107af94bf90d6961
barua-raj <barua.jovraj@gmail.com>
2022-01-02 02:07:47 +0600

    even-odd program added

---
+++ fundamental_computer_programs/even-odd.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Program Name: even-odd</title>
+	</head>
+	<body>
+	
+		<?php
+			
+			$num1 = 257;
+			$num2 = 330;
+			
+			if ( $num1%2==0 ) {
+				echo '<h1>'.$num1.' '.'is an even number.</h1>';
+			}
+			else {
+				echo '<h1>'.$num1.' '.'is an odd number.</h1>';
+			}
+			
+			if ( $num2%2==0 ) {
+				echo '<h1>'.$num2.' '.'is an even number.</h1>';
+			}
+			
+		?>
+	
+	</body>
+</html>
