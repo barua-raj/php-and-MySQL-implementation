d3ffa38fd12c8e85eb56b808042201b396ac7d61
barua-raj <barua.jovraj@gmail.com>
2022-01-02 02:09:05 +0600

    farhenheit-and-celsius program added

---
+++ fundamental_computer_programs/farhenheit-and-celsius.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Program Name: Fahrenhiet and Celsius</title>
+	</head>
+	<body>
+		
+		<?php
+			$fah = 626;
+			$cel = (($fah-32)*5)/9;
+			
+			echo '<h2>The farhenheit temperature '.$fah.' is equivalent to '.$cel.' degree celsius.<br></h2>';
+			
+			$cel = 330;
+			$fah = ((9*$cel)/5)+32;
+			
+			echo '<h2>The celsius temperature '.$cel.' is equivalent to '.$fah.' degree fahrenheit.</h2>';
+			
+		?>
+	</body>
+</html>
