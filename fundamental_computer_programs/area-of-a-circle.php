55ad664d8b1d127067e523ce06a07d99cc59c052
barua-raj <barua.jovraj@gmail.com>
2022-01-02 02:04:14 +0600

    area-of-a-circle program added

---
+++ fundamental_computer_programs/area-of-a-circle.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Program Name: Area of a circle</title>
+	</head>
+	<body>
+		
+		<?php
+			$pi = 3.14;
+			$radius = 5;
+			$area_circle = ($pi**2)*$radius;
+			
+			echo '<h1>Area of a circle of radius '.$radius.' is '.$area_circle.'.</h1>';
+		?>
+	</body>
+	
+</html>
